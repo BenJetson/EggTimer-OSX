af61762f81c7663a75e89f0fe75da2cddcb1ca15
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-05-02 01:02:43 -0400

    Implement preferences window model and controller.

--- EggTimer/View Controllers/PrefsViewController.swift
+++ EggTimer/View Controllers/PrefsViewController.swift
@@ -13,17 +13,67 @@ class PrefsViewController: NSViewController {
     @IBOutlet weak var customSlider: NSSlider!
     @IBOutlet weak var customTextField: NSTextField!
     
+    var prefs = Preferences()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        showExistingPrefs()
+    }
+    
+    func showExistingPrefs() {
+        let selectedTimeInMinutes = Int(prefs.selectedTime) / 60
+        
+        presetsPopup.selectItem(withTitle: "Custom")
+        customSlider.isEnabled = true
+        
+        for item in presetsPopup.itemArray {
+            if item.tag == selectedTimeInMinutes {
+                presetsPopup.select(item)
+                customSlider.isEnabled = false
+                break
+            }
+        }
+        
+        customSlider.integerValue = selectedTimeInMinutes
+        showSliderValueAsText()
+    }
+    
+    func showSliderValueAsText() {
+        let newTimerDuration = customSlider.integerValue
+        let minutesDescription = (newTimerDuration == 1) ? "minute" : "minutes"
+        customTextField.stringValue = "\(newTimerDuration) \(minutesDescription)"
+    }
+    
+    func saveNewPrefs() {
+        prefs.selectedTime = customSlider.doubleValue * 60
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "PrefsChanged"), object: nil)
     }
     
     @IBAction func popupValueChanged(_ sender: Any) {
+        let newTimerDuration = presetsPopup.selectedTag()
+
+        if newTimerDuration == 0 { // if custom is selected
+            customSlider.isEnabled = true
+            return
+        }
+        
+        customSlider.isEnabled = false
+        customSlider.integerValue = newTimerDuration
+        showSliderValueAsText()
     }
+    
     @IBAction func sliderValueChanged(_ sender: Any) {
+        showSliderValueAsText()
     }
+    
     @IBAction func cancelButtonClicked(_ sender: Any) {
+        view.window?.close()
     }
+    
     @IBAction func okButtonClicked(_ sender: Any) {
+        saveNewPrefs()
+        view.window?.close()
     }
 }
